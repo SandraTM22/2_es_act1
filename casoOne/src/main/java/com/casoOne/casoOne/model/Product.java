1690adea626d715fcb64ece2688da5c6bf36e72e
Sandra <delatorremartinezsandra@gmail.com>
2024-10-08 18:44:15 +0200

    Añadimos dependencia JPA. Mapeamos identidades y tablas

--- casoOne/src/main/java/com/casoOne/casoOne/model/Product.java
+++ casoOne/src/main/java/com/casoOne/casoOne/model/Product.java
@@ -1,17 +1,33 @@
 package com.casoOne.casoOne.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//Le decimos que es una entidad
+@Entity
+
+// Le indicamos que esto sera una tabla
+@Table(name = "products")
 public class Product {
 
+    // Le dicemos que es un Id y que tiene que generar un valor unico
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
     private double price;
+    private User user;
 
-    public Product(Integer id, String name, String description, double price) {
+    public Product(Integer id, String name, String description, double price, User user) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.user = user;
     }
 
     public Product() {
@@ -49,11 +65,17 @@ public class Product {
         this.price = price;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Override
     public String toString() {
         return "Producto [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
     }
-
-    
 
 }
