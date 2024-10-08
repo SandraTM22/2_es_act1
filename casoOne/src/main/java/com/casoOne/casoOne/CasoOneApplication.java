d7e291229080cc2f1c7f7dcc40095624e66e1d99
Sandra <delatorremartinezsandra@gmail.com>
2024-10-08 17:48:57 +0200

    First Commit

---
+++ casoOne/src/main/java/com/casoOne/casoOne/CasoOneApplication.java
@@ -0,0 +1,13 @@
+package com.casoOne.casoOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoOneApplication.class, args);
+	}
+
+}
---
+++ casoOne/src/test/java/com/casoOne/casoOne/CasoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casoOne.casoOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
