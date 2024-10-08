235b49876ac0a161e88a3b7aabaa65d17ec51fcc
Sandra <delatorremartinezsandra@gmail.com>
2024-10-08 18:13:52 +0200

    Creación de estructura y modelo Producto

---
+++ casoOne/src/main/java/com/casoOne/casoOne/model/Producto.java
@@ -0,0 +1,59 @@
+package com.casoOne.casoOne.model;
+
+public class Producto {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Producto(Integer id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Producto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + "]";
+    }
+
+    
+
+}
